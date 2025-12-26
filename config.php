c12c5d54ae46c22b3de433d789c766edc61b694c
CIH <denba1120@gmail.com>
2025-12-26 20:51:17 +0000

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-$sso_config = [
-    'client_id'     => 'YOUR_CLIENT_ID_HERE',
-    'client_secret' => 'YOUR_CLIENT_SECRET_HERE',
-    'redirect_uri'  => 'http://your-website.com/login.php', // Must match VATSIM setting exactly
-    'base_url'      => 'https://auth.vatsim.net' // Use https://auth-dev.vatsim.net for testing if needed
-];
-?>
